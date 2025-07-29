15921464884f3ff2bde5709b1bba5fa883b42d17
Raphael Da Silva <rapha.santi@gmail.com>
2025-07-29 09:10:15 +0100

    refactor: update API call in registerUser function to use axios instance and improve error logging

--- api/login.ts
+++ api/login.ts
@@ -1,16 +1,16 @@
 import axios from 'axios';
 
+const api = axios.create({
+  baseURL: 'http://192.168.1.170:5000',
+  timeout: 5000,
+});
+
 export const registerUser = async (nome: string, email: string, senha: string, tipo: string) => {
   try {
-    const response = await axios.post('http://localhost:5000/', {
-      nome,
-      email,
-      senha,
-      tipo
-    });
+    const response = await api.post('/users', { nome, email, senha, tipo });
     return response.data;
   } catch (error) {
-    console.error('Error registering user:', error);
+    console.error('Erro detalhado:', error.response?.data || error.message);
     throw error;
   }
 };
